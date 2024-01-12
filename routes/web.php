ef45409de98d4d17efab8d500e60ec2bcaca3fbf
kalinggapadelmuhamad <enginerbros@gmail.com>
2024-01-12 19:11:57 +0700

    Update Login,Logout,Register using Fortify

--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.apps.dashboard.dashboard', ['type_menu' => 'dashboard']);
+Route::redirect('/', 'login');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', function () {
+        return view('pages.apps.dashboard.dashboard', ['type_menu' => 'dashboard']);
+    })->name('home');
 });
 
-Route::prefix('auth')->group(function () {
-    Route::get('/login', function () {
-        return view('pages.auth.auth-login');
-    })->name('login');
+// Route::prefix('auth')->group(function () {
+//     Route::get('/login', function () {
+//         return view('pages.auth.auth-login');
+//     })->name('login');
 
-    Route::get('/register', function () {
-        return view('pages.auth.auth-register');
-    })->name('register');
+//     Route::get('/register', function () {
+//         return view('pages.auth.auth-register');
+//     })->name('register');
 
-    Route::get('/forgot', function () {
-        return view('pages.auth.auth-forgot-password');
-    })->name('forgot');
+//     Route::get('/forgot', function () {
+//         return view('pages.auth.auth-forgot-password');
+//     })->name('forgot');
 
-    Route::get('/reset', function () {
-        return view('pages.auth.auth-reset-password');
-    })->name('reset');
-});
+//     Route::get('/reset', function () {
+//         return view('pages.auth.auth-reset-password');
+//     })->name('reset');
+// });
