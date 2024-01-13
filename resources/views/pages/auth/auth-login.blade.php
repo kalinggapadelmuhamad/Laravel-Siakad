a232508ee22980df82b00c530a9df93d98cfe906
kalinggapadelmuhamad <enginerbros@gmail.com>
2024-01-14 00:06:38 +0700

    Update Meta tag policy for ngrok

--- resources/views/pages/auth/auth-login.blade.php
+++ resources/views/pages/auth/auth-login.blade.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>Login &mdash; Backend Siakad</title>
 
         <!-- General CSS Files -->
