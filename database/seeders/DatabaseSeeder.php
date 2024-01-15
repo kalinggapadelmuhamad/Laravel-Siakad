75a4a99a6bb428a488789b94b70cf1663b0867ab
kalinggapadelmuhamad <enginerbros@gmail.com>
2024-01-15 14:19:19 +0700

    Update Final Api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -25,6 +26,7 @@ class DatabaseSeeder extends Seeder
             ScheduleSeeder::class,
             KhsSeeder::class,
             AbsensiMatkulSeeder::class,
+            StudentSchedule::class,
         ]);
     }
 }
